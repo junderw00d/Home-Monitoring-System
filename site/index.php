e345b7f6fd191484786e5db6b0dd7f6061eeda51
KoalaMuffin <jcu123123@gmail.com>
2017-04-14 15:02:03 -0400

    Create index.php

---
+++ site/index.php
@@ -0,0 +1,59 @@
+
+<html>
+	<head>
+		<title>
+			Web Portal
+		</title>
+	</head>
+	<body>
+		<center>
+			<h1>
+				Underwood Home Monitoring System Web Portal
+			<h1>
+		</center>
+		<table>
+			<tr>
+				<td>
+					<a href="live.php">
+						Live Camera Feed
+					</a>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<a href="camera">
+						Image Archive
+					</a>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<a href="temp.php">
+						Server Temp
+					</a>
+				</td>
+			</tr>
+		</table>
+		<p>
+			Please contact the webmaster for information on the home monitoring system.
+		</p>
+		<style>
+			body
+			{
+				background-color:a7b7af;
+			}
+			h1
+			{
+				font-family: "Lucida Console";
+			}
+			td
+			{
+				border: 1px solid black;
+			}
+			table
+			{
+				border-collapse: collapse;
+			}
+		</style>
+	</body>
+</html>
