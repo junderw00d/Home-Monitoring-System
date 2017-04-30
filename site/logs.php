b5f78bd5cd0bb49170064d5a9f355fa79baedeb9
KoalaMuffin <jcu123123@gmail.com>
2017-04-30 14:05:04 -0400

    Update logs.php

--- site/logs.php
+++ site/logs.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("127.0.0.1", "root", "UNDERWOODSYSTEM2017", "logs");
+$mysqli = new mysqli("127.0.0.1", "root", "(a-password)", "logs");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
